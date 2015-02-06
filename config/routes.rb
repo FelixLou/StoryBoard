0daba23afad11eedb49e6395ec68417e79a5f80b
northcarolinawolf <ywang95@ncsu.edu>
2015-02-06 23:45:39 +0000

    2nd version of signup and login and log out!

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root                 'sessions#new'
+  get 'about' => 'static_pages#about'
+
+  get 'help' => 'static_pages#help'
+  get 'contact' => 'static_pages#contact'
+
+  root                 'static_pages#home'
   
   get   'login' => 'sessions#new'
   post   'login' =>'sessions#create'
