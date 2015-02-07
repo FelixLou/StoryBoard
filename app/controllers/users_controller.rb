160daa77c7929b08561d060bd58b4e3939de4462
northcarolinawolf <ywang95@ncsu.edu>
2015-02-07 02:35:32 +0000

    add admin to users table, so that admin can delete and users!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index,:edit,:update,:show,:destory]
+  before_action :correct_user, only: [:edit,:update]
+  
+  def index
+    @users=User.all
+  end
+  
   def new
     @user=User.new
   end
   
   def create
     @user=User.new(user_params)
+    @user.admin=true if params[:admin]
     if @user.save
-      flash[:success]="Welcome!"
-      redirect_to @user
+      flash[:success]="Successful created!"
+      redirect_to root_path
     else
       render 'new'
     end
@@ -17,8 +25,41 @@ class UsersController < ApplicationController
     @user=User.find(params[:id])
   end
   
+  def edit
+    @user=User.find(params[:id])
+  end
+  
+  def update
+    @user=User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success]="Profile updated!"      
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success]="User deleted"
+    redirect_to users_url
+  end
+  
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    puts "--------- "+params[:admin]+" -----------"
+    params.require(:user).permit(:name, :email, :password, :admin)
+  end
+  
+  def logged_in_user
+    unless logged_in?
+      flash[:danger]="Pleasee log in."
+      redirect_to login_path
+    end
+  end
+  
+  def correct_user
+    @user=User.find(params[:id])
+    redirect_to(current_user) unless current_user?(@user)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205212511) do
+ActiveRecord::Schema.define(version: 20150207010200) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150205212511) do
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "admin"
   end
 
 end
